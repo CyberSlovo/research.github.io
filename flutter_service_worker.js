6769bd569c232af881e18af411289ddf04624017
Rakhimyan <rahimtoy@gmail.com>
2022-06-16 15:55:33 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e0bfeb8ed4486e589e8b5e1f2d2ff094",
-"index.html": "ed7a249980045de2c5d7a30ed044efb6",
-"/": "ed7a249980045de2c5d7a30ed044efb6",
-"main.dart.js": "b7fdd9cdd5ec47f4a14a54ea252d4a93",
+"index.html": "bf022886712789c6e09c11010942f579",
+"/": "bf022886712789c6e09c11010942f579",
+"main.dart.js": "89b01baed0c05e1a9e9fd784eded67da",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
